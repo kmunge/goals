4a73c7a69834f4353409c62bd72587b1a1f65c28
kmunge <orokomunge@gmail.com>
2019-04-26 17:15:45 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
+import {Goal} from './goal'
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'Goals';
+
 }
